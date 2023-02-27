554c31ecaac89b46ce606867201ffc4eb5d578a8
numa-alset <111363953+numa-alset@users.noreply.github.com>
2023-02-27 20:53:35 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
+from django.conf import settings
+import uuid
 
 
 # Create your models here.
@@ -9,7 +11,11 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
-
+class CarMake(models.Model):
+    car_name = models.CharField(max_length=10)
+    description = models.CharField(max_length=30)
+    def __str__(self):
+        return self.car_name + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,9 +25,24 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
-
+class CarModel(models.Model):
+    CarModel_name = models.CharField(max_length=10)
+    dealerId = models.IntegerField()
+    carmodel = [
+        ('Wagon', 'Wagon'),
+        ('SUV', 'SUV'),
+        ('Sedan', 'Sedan'),
+    ]
+    typeModel = models.CharField(max_length=10,choices=carmodel)
+    year = models.DateField()
+    car = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.CarModel_name
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+
+
+
