6e979389f3248c2f88d18f8b1fade1bf668a29f0
numa-alset <111363953+numa-alset@users.noreply.github.com>
2023-02-28 14:32:26 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -66,30 +66,29 @@ def logout_request(request):
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+
 def registration(request):
     context = {}
     if request.method == 'GET':
-        return render(request, 'djangoapp/user_registration_bootstrap.html', context)
+        return render(request, 'djangoapp/registration.html', context)
     elif request.method == 'POST':
-        # Check if user exists
         username = request.POST['username']
-        password = request.POST['psw']
+        password = request.POST['password']
         first_name = request.POST['firstname']
         last_name = request.POST['lastname']
         user_exist = False
         try:
             User.objects.get(username=username)
             user_exist = True
-        except:
-            logger.error("New user")
+        except Exception as e:
+            logger.error('New user:', e)
         if not user_exist:
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                            password=password)
+            user =  User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
             login(request, user)
-            return redirect("djangoapp:index")
+            return redirect('djangoapp:index')
         else:
             context['message'] = "User already exists."
-            return render(request, 'djangoapp/user_registration_bootstrap.html', context)
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 
